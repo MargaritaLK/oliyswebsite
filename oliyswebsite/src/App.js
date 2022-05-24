2fd6f6f2869d9a2462929ef201ce4d9a54d3050f
Louise Klingen <l.m.klingen@gmail.com>
2022-05-24 13:03:37 +0200

    start website oliys

---
+++ oliyswebsite/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="oliysname">
+          <div>.oliys.</div>
+      </div>
+
+
+      <div className='bio'>
+        <div> @oliys.art</div>
+
+      </div>
+    </div>
+  );
+}
+
+
+
+
+export default App;
