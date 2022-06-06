d20c26e78e7a971c778ee9b2a94ddc48d66212cd
Louise Klingen <l.m.klingen@gmail.com>
2022-06-06 14:19:22 +0200

    setting up page

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='maintitle-container'>
+        <div className="maintitle">.oliys. </div>
+        <a className='subtitle' id='linkstyle' href="https://twitter.com/oliysart" target="_blank">@oliysart</a>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
